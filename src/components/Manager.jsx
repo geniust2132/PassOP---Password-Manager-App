d4b41aee92f16277c2e47ae589aa63ecb0e63d4e
geniust2132 <mhaque2132@gmail.com>
2025-09-19 17:42:40 +0530

    minor changes in toastify

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -227,17 +227,18 @@ const Manager = () => {
 
             <ToastContainer
                 position="top-right"
-                autoClose={5000}
+                autoClose={3000}
                 hideProgressBar={false}
                 newestOnTop={false}
-                closeOnClick={false}
+                closeOnClick
                 rtl={false}
                 pauseOnFocusLoss
                 draggable
                 pauseOnHover
                 theme="light"
-                transition="Bounce"
+                transition={Bounce}   // <-- removed quotes shown problem in deploying
             />
+
 
 
             <div className="absolute inset-0 -z-10 h-full w-full bg-green-100 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div></div>
